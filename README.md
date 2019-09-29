c4505b16161bcd02ae6f276dd06aa501dd94b2a2
Andrii Yashyn <andrii_yashyn@epam.com>
2019-09-29 14:42:33 +0300

    initial

--- README.md
+++ README.md
@@ -1,31 +1,94 @@
-### node-red-contrib-knx
+## node-red-contrib-knx
 
-KNXnet/IP support for NodeRed.
-Uses the KNXnet/IP library for Node.JS (https://www.npmjs.com/package/knx).
+KNXnet/IP support for Node-Red. Provides nodes to talk to KNX both in raw form and as higher level devices.
 
-#### knx-controller
-a hidden CONFIG node that holds connection configuration for eibd and will acts as the encapsulator for KNX access. It cannot be added to a graph, but it acts as a singleton object that gets created in the the background when you add an 'knx' or 'knx-device' node and configure it accordingly.
+Uses the KNXnet/IP library for Node.JS (https://www.npmjs.com/package/knx) which supports multicast.
 
-#### knx-out
 
-KNX/EIB output node that can send KNX to arbitrary GA's and datatypes, so it can be used with function blocks.
+- Easily import your group addresses from a CSV file exported via ETS, and define your devices as individual nodes in a flow or,
+- Handle arbitrary KXN telegrams by using a pair of generic in/out nodes.
 
-#### knx-in
+### Installation
+
+```sh
+cd ~/.node-red
+npm install node-red-contrib-knxjs
+```
+
+### Nodes added by this package
+
+#### - knx-device
+
+A node that represents a controllable KNX device. Provides the  ability to
+- *control* device via node messages that flow into its *input* connector:
+
+```js
+{msg: {payload: true}}
+```
+will turn on a KNX switch
+
+- *inject* messages (via its *output* connector) into a flow when a device status changes.
+
+```js
+{
+    topic: <knx device node name>,
+    payload: {'oldval': <old value>, 'newval': <new value>)
+}
+```
+
+You can define a device by
+- *manually* adding its control and status group address, OR
+- you can import a CSV from ETS that contains all your group addresses.
+
+The latter is preferred whenever you want to define multiple devices for your flows.
+
+
+#### - knx-out
+
+KNX/EIB output node that can send raw KNX telegrams to arbitrary GA's and datatypes, so it can be used with function blocks.
+
+#### - knx-in
 
 KNX/EIB listener node, who emits flow messages based on activity on the KNX bus:
 
-Both use the same message format, an example message follows:
+Both `knx-in` and `knx-out` use the same message format, an example message follows:
 
-{ "topic": "knx: write", "payload": { "srcphy": "1.1.100", "dstgad": "5/0/2", "dpt": "DPT1", "value": 0 } }
+```js
+{ "topic": "knx: write",
+  "payload": {
+    "srcphy": "1.1.11",
+    "dstgad": "0/0/15",
+    "value": [12,65]
+  }
+}
+```
 
- -- topic is: *"knx: (telegram type)" where (telegram type) is 'read' (read requests), 'response' (to read requests) and 'write' (to update GA's)
+Notice: the KNX wire protocol does **not** carry information about the DPT (datapoint type), hence the interpretation of the value is up to you (in the example above, this is a 16-bit floating point representing a temperature in degrees Celcius).
+If you're interested in getting meaningful values out of KNX telegrams, you need to use the `knx-device` node (see above).
 
- -- payload contains:
+ - **topic**: *"knx: (telegram type)"* where (telegram type) is 'read' (for read requests), 'respond' (for responses to read requests) and 'write' (to write GA values)
 
- --- srcphy: source physical address (the device that sent the KNX/EIB telegram) - this information is only emitted by eibd-in, and will be ignored by eibd-out (no address spoofing, you naughty haxx0r!)
+ - **payload.srcphy**: source physical address (the device that sent the KNX/EIB telegram)
 
- --- dstgad: destination group address (the function that this telegram refers to eg. front porch lights) - REQUIRED
+ - **payload.dstgad**: destination group address (the function that this telegram refers to eg. front porch lights)
 
- --- dpt: datapoint type (1 for booleans, 5 for 4-bit dimming setpoints etc) - defaults to 1 for boolean on/off GA's
+ - **dpt**: for write requests, you can optionally specify the datapoint type (eg. `DPT1` for booleans, `DPT5` for 4-bit dimming setpoints etc) - this permits you to *write* complex DPT's to the bus
 
- --- value: the destination group address's value conveyed in the telegram - REQUIRED
+ - **value**: the destination group address's value conveyed in the telegram in raw format (if you're not supplying a dpt), or in the format expected by the DPT (see [relevant table](https://bitbucket.org/ekarak/knx.js/src/master/README-datapoints.md?fileviewer=file-view-default) for what kind of data values you can send to the KNX library).
+
+### Configuration Nodes
+
+#### - knx-controller
+
+A hidden CONFIG node that lets you define connection properties for your KNX installation. Defaults to a **router/multicast** connection to 224.0.23.12, so if you have a KNX IP router on your LAN, you can use this without further ado.
+**Please note**: if you use `eibd` or `knxd` as your IP router, and you have it running on the **same** box as your Node-Red, it will DROP multicast packets coming from the same source IP address.
+
+`Layer 0 [11:server/Server     7.133] Dropped(017): 06 10 05 30 00 11 29 00 BC D0 11 64 29 0F 01 00 80`
+
+The trick here (although not entirely within the specs) is to use the loopback interface, so if you define the KNX controller address/port to 127.0.0.1:3671 it will bypass the source address check (and happily route your packets down your USB or TPUART interface)
+
+
+#### - ets-csv-export
+
+An encapsulator object for your CSV file of your exported group addresses. You can
+define this in order to be able to pick a group address in a 'knx-device' node.
--- knxnetip/knxnetip.js
+++ knxnetip/knxnetip.js
@@ -1,141 +1,206 @@
-/*
-  KNX nodes for IBM's Node-Red
-  https://github.com/ekarak/node-red-contrib-KNX
-  (c) 2016, Elias Karakoulakis <elias.karakoulakis@gmail.com>
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
 module.exports = function(RED) {
-
-	console.log("loading KNXnet/IP support for NodeRed");
-	var knx = require('knx');
+  var fs = require('fs');
+  var path = require('path');
   var util = require('util');
-	/**
-	* ====== KNX-CONTROLLER ================
-	* Holds configuration for KNX host+port,
-	* initializes new KNX connections
-	* =======================================
-	*/
-	function KNXControllerNode(config) {
-    var node = this;
-		RED.nodes.createNode(this, config);
-    this.config = config;
-		/**
-		* Initialize an KNX connection
-		*/
-    node.log(util.format('KNX: initializing %s connection to %s:%d',
-      config.conntype, config.ipAddr, config.ipPort));
-    switch(config.conntype) {
-      case "multicast":
-        this.connection = knx.IpRoutingConnection(config);
-        break;
-      case "unicast":
-        this.connection = knx.IpTunnelingConnection(config);
-        break;
-    }
-    //this.connection.debug = true;
-    this.connection.Connect(function() {
-      node.log(util.format('KNX: successfully connected to %s:%d',
-        node.config.ipAddr, node.config.ipPort));
-      /* ===== KNX events ===== */
-      // initialize incoming KNX event
-      node.connection.on('event', function (evt, src, dest, value) {
-        node.log(util.format('%s src=%s, dest=%s, val=%s', evt, src, dest, value));
-        node.emit(evt, src, dest, value);
+  var knx = require('knx');
+
+  var idleStatuses = {};
+  var setNodeStatus = function(node, tempstatus) {
+    node.status(tempstatus);
+    setTimeout(function() {
+      var idlestatus = idleStatuses[node.id];
+      if (idlestatus) node.status(idlestatus);
+    }, 1000);
+  };
+
+  var notificationHandler = function(evt, src, dest, value) {
+    //console.trace(' evt=%s', this, evt);
+    // emit the event ourselves
+    this.emit('event', evt, src, dest, value);
+    var payload = {
+      'srcphy': src,
+      'dstgad': dest
+    };
+    var knxevent = evt.match(/GroupValue_(.*)/);
+    if (knxevent) {
+      if (evt == "GroupValue_Write" || evt == "GroupValue_Response") {
+        payload.value = value;
+      }
+      //this.log(util.format('%s %j', evt, payload));
+      // set a temporary node status
+      setNodeStatus(this, {
+        color: 'green',
+        shape: 'dot',
+        text: knxevent[1] + ' ' + dest + ':' + value
       });
-      node.emit('connected');
-		});
-	}
-	RED.nodes.registerType("knx-controller", KNXControllerNode);
+      // and send a Node-Red message
+      this.send({
+        'topic': 'knx: ' + knxevent[1].toLowerCase(),
+        'payload': payload
+      });
+    }
+  };
 
-	/**
-	* ====== KNX-OUT =======================
-	* Sends outgoing KNX telegrams from
-	* messages received via node-red flows
-	* =======================================
-	*/
-	function KNXOut(config) {
-		node.log('KNX: new KNX-OUT node, config: %j', config);
-		RED.nodes.createNode(this, config);
-		this.name = config.name;
-		this.ctrl = RED.nodes.getNode(config.controller);
-		var node = this;
-		//
-		this.on("input", function(msg) {
-			//log('KNXout.onInput, msg=%j', msg);
-			if (!(msg && msg.hasOwnProperty('payload'))) {
-				node.log('KNXout.onInput: illegal msg.payload!');
-				return;
-			}
-			var payload;
-			switch (typeof(msg.payload)) {
-				case 'object': 	payload = msg.payload; break;
-				case 'string':	payload = JSON.parse(msg.payload); break;
-				default:
-					node.log('KNXout.onInput: illegal msg.payload type: '+typeof(msg.payload));
-					return;
-			}
-      this.connection.write(payload.dstgad, payload.value, payload.dpt);
-		});
-    //
-		this.on("close", function() {
-			node.log('KNX: KNXOut.close');
-      if (KNXController.connection) KNXController.connection.disconnect();
-		});
-	}
-	//
-	RED.nodes.registerType("knx-out", KNXOut);
-
-	/**
-	* ====== KNX-IN ========================
-	* Handles incoming KNX events, injecting
-	* json into node-red flows
-	* =======================================
-	*/
-	function KNXIn(config) {
+  /**
+   * ====== KNX-CONTROLLER ================
+   * Holds configuration for KNX host+port,
+   * initializes new KNX connections
+   * =======================================
+   */
+  function KNXControllerNode(config) {
     var node = this;
-		RED.nodes.createNode(this, config);
-		this.name = config.name;
-		this.inconn = null;
-		var KNXController = RED.nodes.getNode(config.controller);
-		/* ===== Node-Red events ===== */
-		this.on("input", function(msg) {
-			if (msg != null) {
+    RED.nodes.createNode(this, config);
+    this.config = config;
+    this.config.ipAddr = this.config.ipAddr || '224.0.23.12';
+    this.config.ipPort = this.config.ipPort || 3671;
+    this.config.physAddr = this.config.physAddr || '15.15.15';
+    /**
+     * Initialize an KNX connection
+     */
+    node.log(util.format('Initializing connection to %s:%d, physical addr: %s',
+      this.config.ipAddr, this.config.ipPort, this.config.physAddr));
 
-			};
-		});
-		this.on("close", function() {
-			node.log('KNXIn.close');
-			if (KNXController.connection) KNXController.connection.Disconnect();
-		});
-//		this.on("error", function(msg) {});
-    node.status({
-      fill: "green",
-      text: "connected",
-      shape:"ring"
+    // get a closure for this instance
+    this.notificationClosure = notificationHandler.bind(this);
+    this.status({
+      fill: "yellow",
+      text: "connecting",
+      shape: "dot"
     });
-    KNXController.on("GroupValue_Write", function(src,dest,value) {
-      node.send({	topic: 'knx: write', payload: {'srcphy': src, 'dstgad': dest, 'value': value }});
-    });
-    KNXController.on("GroupValue_Read", function(src,dest) {
-      node.send({	topic: 'knx: read', payload: {'srcphy': src, 'dstgad': dest }});
-    });
-    KNXController.on("GroupValue_Response", function(src,dest,value) {
-      node.send({	topic: 'knx: response', payload: {'srcphy': src, 'dstgad': dest, 'value': value }});
-    });
-	}
 
-	//
-	RED.nodes.registerType("knx-in", KNXIn);
-}
+    // connect to KNX
+    this.connection = knx.Connection({
+      ipAddr: this.config.ipAddr,
+      ipPort: this.config.ipPort,
+      physAddr: this.config.physAddr,
+      handlers: {
+        connected: function() {
+          node.log(util.format(
+            'KNX: successfully connected to %s:%d as %s',
+            node.config.ipAddr, node.config.ipPort, node.config.physAddr));
+          var status = {
+            fill: "green",
+            text: "connected",
+            shape: "dot"
+          };
+          idleStatuses[node.id] = status;
+          node.status(status);
+          node.emit('connected');
+        },
+        /* ===== bind to KNX events ===== */
+        event: node.notificationClosure,
+        /* ===== cleanup on KNX connection error ===== */
+        error: function(msg) {
+          idleStatuses[this.id] = {
+            fill: "red",
+            text: "connection error: " + msg,
+            shape: "dot"
+          };
+          setNodeStatus(node, idleStatuses[this.id]);
+          node.log(util.format("KNX Connection Error: %s", msg));
+        }
+      }
+    });
+
+    // remove the listener subscription from the FSM on close
+    this.on("close", function() {
+      node.log('Unbinding generic event handler');
+      // Machina.JS uses off() instead of removeListener()
+      this.connection.off("event", this.notificationClosure);
+    });
+  }
+
+  RED.nodes.registerType("knx-controller", KNXControllerNode);
+
+  /**
+   * ====== KNX-OUT =======================
+   * Sends outgoing KNX telegrams from
+   * messages received via node-red flows
+   * =======================================
+   */
+  function KNXOut(config) {
+    var node = this;
+    RED.nodes.createNode(this, config);
+    this.name = config.name;
+    var KNXController = RED.nodes.getNode(config.controller);
+    //
+    this.on("input", function(msg) {
+      //log('KNXout.onInput, msg=%j', msg);
+      if (!(msg && msg.hasOwnProperty('payload'))) {
+        node.log('KNXout.onInput: illegal msg.payload!');
+        return;
+      }
+      var payload;
+      switch (typeof(msg.payload)) {
+        case 'object':
+          payload = msg.payload;
+          break;
+        case 'string':
+          payload = JSON.parse(msg.payload);
+          break;
+        default:
+          node.log('KNXout.onInput: illegal msg.payload type: ' + typeof(
+            msg.payload));
+          return;
+      }
+      if (KNXController && KNXController.connection) {
+        switch(msg.topic) {
+          case 'read':
+          case 'knx: read':
+            KNXController.connection.read(payload.dstgad);
+            break;
+          case 'respond':
+          case 'response' :
+          case 'knx: respond':
+          case 'knx: response':
+            KNXController.connection.respond(payload.dstgad, payload.value, payload.dpt);
+            break;
+          case 'write':
+          case 'knx: write':
+            KNXController.connection.write(payload.dstgad, payload.value, payload.dpt);
+            break;
+        }
+      }
+    });
+  }
+  //
+  RED.nodes.registerType("knx-out", KNXOut);
+
+  /**
+   * ====== KNX-IN ========================
+   * Handles incoming KNX events, injecting
+   * json into node-red flows
+   * =======================================
+   */
+  function KNXIn(config) {
+    var node = this;
+    RED.nodes.createNode(this, config);
+    this.name = config.name;
+    var KNXController = RED.nodes.getNode(config.controller);
+
+    node.log('Binding KNX-In event handler');
+    this.notificationClosure = notificationHandler.bind(this);
+    KNXController.on("event", this.notificationClosure);
+    KNXController.on("connected", function() {
+      idleStatuses[node.id] = {
+        fill: "green",
+        text: "connected",
+        shape: "ring"
+      };
+      setNodeStatus(node, idleStatuses[node.id]);
+    });
+
+    // remove the listener subscription on close
+    this.on("close", function() {
+      node.log('KNXIn.close - Unbinding generic event handler');
+      KNXController.removeListener("event", this.notificationClosure);
+    });
+
+    this.on("error", function(msg) {
+      node.log('KNXIn.error - ' + msg);
+    });
+  }
+
+  //
+  RED.nodes.registerType("knx-in", KNXIn);
+};
